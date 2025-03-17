949077820ae2eb0f910834d0ef73d1f53a4d6da0
remmy-web <137243748+remmy-web@users.noreply.github.com>
2025-03-17 13:12:21 +0300

    ready for deployment1

--- webstar/webstar/settings.py
+++ webstar/webstar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(&g8n-h*rp_0tc*$ec2=2xnubm909h$9$dv!5f@!a_l)(r#h)7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -121,6 +121,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
